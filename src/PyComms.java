2c7d77a25372e7545b70831765cc833ec926dabf
CSstudent CSstudent <CSstudent@warrenhills.org>
2016-04-05 13:18:50 -0400

    Fixed to print one line at a time and only one time instead of multiple

--- src/PyComms.java
+++ src/PyComms.java
@@ -3,7 +3,9 @@ import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+import javax.swing.JDialog;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JTextPane;
 
 public class PyComms {
@@ -11,27 +13,29 @@ public class PyComms {
 	
 	public static void main(String[] args) {
 	
+		String prev = "";
+		
+		JOptionPane occ = new JOptionPane("WARNING",JOptionPane.WARNING_MESSAGE);
+		JDialog dlg = occ.createDialog("OCCUPIED");
 		
 		try{
 			ServerSocket server = new ServerSocket(4444);
 			System.out.println("Waiting for client on port 4444");
+			Socket connected = server.accept();
+			System.out.println("CONNECTED WITH CLIENT");
 			while(true){
-				Socket connected = server.accept();
-				System.out.println("CONNECTED WITH CLIENT");
 				BufferedReader inFromPi = new BufferedReader(new InputStreamReader(connected.getInputStream()));
-				while(true){
-					String fromclient = inFromPi.readLine();
-					
-					if(fromclient.equalsIgnoreCase("Occupied")){
-						System.out.println("Client responded with "+fromclient + "\n");
-					}
-					else{
-						System.out.println("Client responded with "+fromclient + "\n");
-						connected.close();
-					}
+				String fromclient = inFromPi.readLine();
+				
+				if(prev.equalsIgnoreCase(fromclient));
+				else{
+					System.out.println("Client responded with "+fromclient);
 				}
 				
+				prev = fromclient;
+				
 			}	
+			
 			
 		}
 		catch(Exception e){
